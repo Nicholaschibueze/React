603799db3f6e62952d17c3d6dce60f5c1e936bca
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2022-02-26 13:57:01 +0100

    worked on profile

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 // import logo from './logo.svg';
  import './App.css';
  import React from 'react';
+import Profile from './component/Profile';
 // import Profile from './component/Profile';
 // import Img from './component/Img';
 // import Mystyle from './stylesheet/Mystyle';
 
 
- 
-
-
-
-
-
-
-
-
 function App() {
-return( <div class="form-container">
-<form class="register-form">
-  {/* Uncomment the next line to show the success message */}
-  {/* <div class="success-message">Success! Thank you for registering</div> */}
-  <input
-    id="first-name"
-    class="form-field"
-    type="text"
-    placeholder="First Name"
-    name="firstName"
-  />
-  {/* Uncomment the next line to show the error message */}
-  {/* <span id="first-name-error">Please enter a first name</span> */}
-  <input
-    id="last-name"
-    class="form-field"
-    type="text"
-    placeholder="Last Name"
-    name="lastName"
-  />
-  {/* Uncomment the next line to show the error message */}
-  {/* <span id="last-name-error">Please enter a last name</span> */}
-  <input
-    id="email"
-    class="form-field"
-    type="text"
-    placeholder="Email"
-    name="email"
-  />
-  {/* Uncomment the next line to show the error message */}
-  {/* <span id="email-error">Please enter an email address</span> */}
-  <button class="form-field" type="submit">
-    Register
-  </button>
-</form>
+return( <div>
+<Profile/>
 </div>
 );
 }
--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,11 +1,36 @@
 import React from "react";
-function Profile() {
-    return(
-      <div>
-      Profile
-      </div>
+import '../stylesheet/style.css';
+import  '../assets/man.png';
 
-      
+function Profile() {
+    return( 
+    <div class="container">
+    <div class="profileImg">
+    <img src={require('./man.png')} alt='' />
+
+</div>   
+    <div class="inputCont">
+        <span>Name:</span>
+        <input type="text"/>
+    
+    </div>
+    <div class="inputCont">
+        <span>Stack:</span>
+        <input type="text"/>
+    
+    </div>
+    <div class="inputCont">
+        <span>Contact:</span>
+        <input type="number"/>
+    
+    </div>
+    
+  <div class="btn"> 
+    <button>Toggle Backgrond</button>
+  </div>
+  <assets/>
+
+ </div>
     )
     
 
